95ffedb09d68852dee503212cb726f0f65172375
Adriano Faria Alves <adriano.faria@outlook.com.br>
2025-01-28 19:44:41 -0300

    Simplify footer content in MainLayout by removing toolbar and adding copyright notice

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -40,14 +40,7 @@
     </q-page-container>
 
     <q-footer reveal bordered class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-          <div>Title</div>
-        </q-toolbar-title>
-      </q-toolbar>
+      <div class="text-center q-pa-sm">&copy; 2023 Your Company. All rights reserved.</div>
     </q-footer>
   </q-layout>
 </template>
